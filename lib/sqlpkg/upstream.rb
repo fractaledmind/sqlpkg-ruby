572c5a2317dcebf9e49036dbef5bed080e798387
Stephen Margheim <stephen.margheim@gmail.com>
2023-12-21 00:18:24 +0800

    bundle exec standardrb --fix

--- lib/sqlpkg/upstream.rb
+++ lib/sqlpkg/upstream.rb
@@ -8,7 +8,7 @@ module Sqlpkg
       "arm64-darwin" => "sqlpkg-cli_#{VERSION}_darwin_arm64.tar.gz",
       "arm64-linux" => "sqlpkg-cli_#{VERSION}_linux_arm64.tar.gz",
       "x86_64-darwin" => "sqlpkg-cli_#{VERSION}_darwin_amd64.tar.gz",
-      "x86_64-linux" => "sqlpkg-cli_#{VERSION}_linux_amd64.tar.gz",
+      "x86_64-linux" => "sqlpkg-cli_#{VERSION}_linux_amd64.tar.gz"
     }
   end
 end
